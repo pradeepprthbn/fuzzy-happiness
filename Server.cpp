db86ad168c5a25f2827fc936381039d01b3b08fe
Pradeep Parthiban <pradeepprthbn@gmail.com>
2020-06-22 13:58:32 +0200

    Add files via upload

--- Server.cpp
+++ Server.cpp
@@ -1,125 +1,100 @@
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-#include <utility>
-#include <boost/asio.hpp>
-#include <boost/lexical_cast.hpp>
+#include "Server.h"
 
-#define _WIN32_WINNT 0x0601
-
-using boost::asio::ip::tcp;
-
-class session
-  : public std::enable_shared_from_this<session>
+session::session(tcp::socket socket) :
+  socket_(std::move(socket))
 {
-public:
-  session(tcp::socket socket)
-    : socket_(std::move(socket))
-  {
-  }
+}
 
-  void start()
-  {
-    do_read();
-  }
-
-private:
-  void do_read()
-  {
-    auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-      [this, self](boost::system::error_code ec, std::size_t length)
-    {
-      if (!ec)
-      {
-        do_calculate();
-      }
-    });
-  }
-
-  void do_write(std::size_t length)
-  {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-      [this, self](boost::system::error_code ec, std::size_t /*length*/)
-    {
-      if (!ec)
-      {
-        do_read();
-      }
-    });
-  }
-
-  void do_calculate()
-  {
-
-    std::string buffer = data_;
-    buffer.pop_back();
-
-    uint32_t value = UINT32_MAX;
-
-    std::string reply;
-    char sstring[50] = "Number received and the Max value";
-    char number[10];
-
-    sprintf(number, "%u", value);
-    reply = strcat(sstring, number);
-
-    uint32_t numberToCheck = boost::lexical_cast<uint32_t>(buffer);
-
-    if (numberToCheck % 2 == 0)
-    {
-      sum = sum + numberToCheck;
-      std::cout << "Number received is even and value" << " : " << sum << "\n";
-    }
-    else
-    {
-      sum = sum - numberToCheck;
-      std::cout << "Number received is odd and value" << " : " << sum << "\n";
-    }
-
-    if (sum < UINT32_MAX)
-    {
-      do_write(max_length);
-    }
-    else
-    {
-      socket_.close();
-    }
-  }
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-  uint32_t sum;
-};
-
-class server
+server::server(boost::asio::io_context & io_context, short port) :
+  acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
 {
-public:
-  server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  do_accept();
+}
+
+void session::start()
+{
+  do_read();
+}
+
+void session::do_read()
+{
+  auto self(shared_from_this());
+  socket_.async_read_some(boost::asio::buffer(data_, max_length),
+    [this, self](boost::system::error_code ec, std::size_t length)
   {
+    if (!ec)
+    {
+      do_calculate();
+    }
+  });
+}
+
+void session::do_write(std::size_t length)
+{
+  auto self(shared_from_this());
+  boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+    [this, self](boost::system::error_code ec, std::size_t /*length*/)
+  {
+    if (!ec)
+    {
+      do_read();
+    }
+  });
+}
+
+void session::do_calculate()
+{
+
+  std::string buffer = data_;
+  buffer.pop_back();
+
+  uint32_t value = UINT32_MAX;
+
+  std::string reply;
+  char sstring[50] = "Number received and the Max value";
+  char number[10];
+
+  sprintf(number, "%u", value);
+  reply = strcat(sstring, number);
+
+  uint32_t numberToCheck = boost::lexical_cast<uint32_t>(buffer);
+
+  if (numberToCheck % 2 == 0)
+  {
+    sum = sum + numberToCheck;
+    std::cout << "Number received is even and value" << " : " << sum << "\n";
+  }
+  else
+  {
+    sum = sum - numberToCheck;
+    std::cout << "Number received is odd and value" << " : " << sum << "\n";
+  }
+
+  if (sum < UINT32_MAX)
+  {
+    do_write(max_length);
+  }
+  else
+  {
+    socket_.close();
+  }
+}
+
+void server::do_accept()
+{
+  acceptor_.async_accept(
+    [this](boost::system::error_code ec, tcp::socket socket)
+  {
+    if (!ec)
+    {
+      std::make_shared<session>(std::move(socket))->start();
+    }
+
     do_accept();
-  }
+  });
+}
 
-private:
-  void do_accept()
-  {
-    acceptor_.async_accept(
-      [this](boost::system::error_code ec, tcp::socket socket)
-    {
-      if (!ec)
-      {
-        std::make_shared<session>(std::move(socket))->start();
-      }
 
-      do_accept();
-    });
-  }
-
-  tcp::acceptor acceptor_;
-};
 
 int main(int argc, char* argv[])
 {
---
+++ Server.h
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
+
+#define max_length 1024
+
+using boost::asio::ip::tcp;
+
+class server
+{
+public:
+  server(boost::asio::io_context& , short );
+
+private:
+  void do_accept();
+
+  tcp::acceptor acceptor_;
+};
+
+class session
+  : public std::enable_shared_from_this<session>
+{
+public:
+  session(tcp::socket );
+  void start();
+
+private:
+  void do_read();
+  void do_write(std::size_t);
+  void do_calculate();
+  
+  tcp::socket socket_;
+  char data_[max_length];
+  uint32_t sum;
+
+};
+
+
